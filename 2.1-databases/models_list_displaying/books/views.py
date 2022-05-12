09b6df147a9a36562f19ebba40b79c7b53d9aa32
Rinat <94633754+Ulitka163@users.noreply.github.com>
2022-05-12 22:25:57 +0400

    ver_01 2.1-databases\models_list_displaying

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from books.models import Book
+
 
 def books_view(request):
+    books_objects = Book.objects.all()
     template = 'books/books_list.html'
-    context = {}
+    context = {
+        'books': books_objects
+    }
     return render(request, template, context)
