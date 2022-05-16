48784b7c20d4731c660ea4a7c06acd1d25849e87
Rinat <94633754+Ulitka163@users.noreply.github.com>
2022-05-16 19:56:08 +0400

    ver 01  2.2-databases-2/orm_migrations

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -76,6 +78,8 @@ DATABASES = {
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'afybz1987'
     }
 }
 
@@ -102,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -131,3 +135,7 @@ try:
 except ImportError:
     pass
 
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
