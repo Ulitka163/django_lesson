a6171f698ac83e3fb369142f575a05128ffef916
Rinat <94633754+Ulitka163@users.noreply.github.com>
2022-05-24 21:49:16 +0400

    ver_02 2.1-databases\models_list_displaying

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
@@ -10,3 +14,23 @@ def books_view(request):
         'books': books_objects
     }
     return render(request, template, context)
+
+
+def book_view(request, pub_date):
+    books_objects = Book.objects.all()
+    back_pub_date = Book.objects.exclude(pub_date__gte=pub_date).order_by('-pub_date').first()
+    next_pub_date = Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first()
+    print(back_pub_date, next_pub_date)
+    book_list = []
+    for book in books_objects:
+        if str(book.pub_date) == pub_date:
+            book_list.append(book)
+    print(book_list)
+    template = 'books/book.html'
+    context = {
+        'book_list': book_list,
+        'back': back_pub_date,
+        'next': next_pub_date,
+    }
+    return render(request, template, context)
+
